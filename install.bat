4059700041f69ad99c33cedea96dc533227c0a3b
jixiong398-blip <jixiong398@users.noreply.github.com>
2026-05-25 23:13:13 +0800

    v0.4.2: fix encoding - add chcp 65001 + PYTHONIOENCODING to all .bat

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
 @echo off
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
 title QQBot Installer
 cd /d "%~dp0"
 cls
--- setup.bat
+++ setup.bat
@@ -1,3 +1,5 @@
 @echo off
+chcp 65001 >nul
+set PYTHONIOENCODING=utf-8
 cd /d "%~dp0"
 .venv\Scripts\python.exe scripts\setup_config.py
